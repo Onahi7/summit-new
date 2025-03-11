74cecc40c82727bd973a64ed6e5295e406db6be3
Onahi7 <dicksonhardy7@gmail.com>
2025-03-11 18:43:43 +0100

    tired

--- app/payment/layout.tsx
+++ app/payment/layout.tsx
@@ -1,7 +1,11 @@
+"use client"
+
+import { ParticipantAuthProvider } from "@/components/participant-auth-provider"
+
 export default function PaymentLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return children
+  return <ParticipantAuthProvider>{children}</ParticipantAuthProvider>
 }
